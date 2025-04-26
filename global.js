d1e340fa8e36c5719f309f93a274ea668e5e5a5e
RyanCao <GalaxyBrain2@users.noreply.github.com>
2025-04-25 19:04:45 -0700

    lab 4 steps

--- global.js
+++ global.js
@@ -59,3 +59,34 @@ document.body.insertAdjacentHTML(
   select.addEventListener('input', function (event) {
     setColorScheme(event.target.value);
   });
+  // projects page functions
+  export async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
+  export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    // Your code will go here
+    containerElement.innerHTML = '';
+    project.forEach(project => {
+      const article = document.createElement('article');
+      article.innerHTML = `
+        <${headingLevel}>${project.title} (${project.year})</${headingLevel}>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>
+        `;
+      containerElement.appendChild(article);
+    });
+  };
+  export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+  }
