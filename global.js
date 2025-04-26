9deb3e9d2a8bae838d26a97400d2023243dfc5f2
RyanCao <GalaxyBrain2@users.noreply.github.com>
2025-04-25 20:20:12 -0700

    updated global.js

--- global.js
+++ global.js
@@ -61,32 +61,26 @@ document.body.insertAdjacentHTML(
   });
   // projects page functions
   export async function fetchJSON(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Failed to fetch projects: ${response.statusText}`);
-        }
-        const data = await response.json();
-
-        return data;
-    } catch (error) {
-      console.error('Error fetching or parsing JSON data:', error);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
     }
+    const data = await response.json();
+    return data;
   }
-
-  export function renderProjects(project, containerElement, headingLevel = 'h2') {
-    containerElement.innerHTML = '';
-    
-    project.forEach(project => {
+  
+  export function renderProjects(projects, container, headingLevel = 'h2') {
+    container.innerHTML = "";
+    for (let project of projects) {
       const article = document.createElement('article');
       article.innerHTML = `
-        <${headingLevel}>${project.title} (${project.year})</${headingLevel}>
+        <${headingLevel}>${project.title}</${headingLevel}>
         <img src="${project.image}" alt="${project.title}">
         <p>${project.description}</p>
-        `;
-      containerElement.appendChild(article);
-    });
-  };
-  export async function fetchGitHubData(username) {
+      `;
+      container.appendChild(article);
+    }
+  }
+  export async function fetchGithubData(username) {
     return fetchJSON(`https://api.github.com/users/${username}`);
   }
