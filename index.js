59c6eb9995e2c12ba10f737cd2157efb3923e719
RyanCao <GalaxyBrain2@users.noreply.github.com>
2025-04-25 19:28:59 -0700

    updated home page

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h3');
 
-const githubData = await fetchGitHubData('RyanCao2005');
+const githubData = await fetchGitHubData('GalaxyBrain2');
 const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
     profileStats.innerHTML = `
           <dl>
-            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
             <dt>Followers:</dt><dd>${githubData.followers}</dd>
             <dt>Following:</dt><dd>${githubData.following}</dd>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            
           </dl>
       `;
   }
